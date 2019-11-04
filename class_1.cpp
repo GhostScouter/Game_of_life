b6c10d2051902818a1c5f41563d28c6a38603fd8
GhostScouter <48020801+GhostScouter@users.noreply.github.com>
2019-11-04 22:17:48 +0100

    evolution starting

--- class_1.cpp
+++ class_1.cpp
@@ -85,11 +85,223 @@ int zellulaerer_automat::veraendere_feld()
 
 int zellulaerer_automat::evolution()
 {
-    int n;
+    int n, j;
+    int zellen_status;
+    int counter = 0;
+
+    delete koor_feld_neu;
+    koor_feld_neu = new int[(neue_hoehe*neue_breite)];
 
     for (n=0; n<(neue_hoehe*neue_breite); n++)
     {
+        if (n == 0) // ecke links oben
+        {
+            zellen_status = koor_feld_alt[n];
 
+            if (koor_feld_alt[neue_hoehe*neue_breite-1] == 1)
+            {
+                counter += 1;
+            }
+            for (j = (neue_hoehe*neue_breite-neue_breite); j < ((neue_hoehe*neue_breite-neue_breite)+2); j++)
+            {
+                if (koor_feld_alt[j] == 1)
+                {
+                    counter += 1;
+                }
+            j = 0;
+            }
+            if (koor_feld_alt[neue_breite-1] == 1)
+            {
+                counter += 1;
+            }
+            if (koor_feld_alt[1] == 1)
+            {
+                counter += 1;
+            }
+            if (koor_feld_alt[2*neue_breite-1] == 1)
+            {
+                counter += 1;
+            }
+            for (j = (neue_breite); j < (neue_breite+2); j++)
+            {
+                if (koor_feld_alt[j] == 1)
+                {
+                    counter += 1;
+                }
+            j = 0;
+            }
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else if (n == (neue_breite - 1)) // ecke rechts oben
+        {
+            zellen_status = koor_feld_alt[n];
+
+            for (j = (neue_breite*neue_hoehe-2); j < (neue_breite*neue_hoehe); j++)
+            {
+                if (koor_feld_alt[j] == 1)
+                {
+                    counter += 1;
+                }
+            j = 0;
+            }
+            if (koor_feld_alt[neue_hoehe*neue_breite-neue_breite] == 1)
+            {
+                counter += 1;
+            }
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else if (n == (neue_hoehe*neue_breite-neue_breite)) // ecke links unten
+        {
+            zellen_status = koor_feld_alt[n];
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else if (n == (neue_breite*neue_hoehe - 1)) // ecke rechts unten
+        {
+            zellen_status = koor_feld_alt[n];
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else if (n>0 && n<(neue_breite-1)) // obere Zeile (ohne Ecken)
+        {
+            zellen_status = koor_feld_alt[n];
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else if (n % (neue_breite) == 0 && n != 0 && n!= (neue_hoehe*neue_breite-neue_breite)) // linke Seite (ohne Ecken)
+        {
+            zellen_status = koor_feld_alt[n];
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else if (n % (neue_breite) == (neue_breite-1) && n!=(neue_breite - 1) && n!=(neue_breite*neue_hoehe - 1)) // rechte Seite (ohne Ecken)
+        {
+            zellen_status = koor_feld_alt[n];
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else if (n> neue_breite*neue_hoehe-neue_breite && n<neue_hoehe*neue_breite) // untere Zeile (ohne Ecken)
+        {
+            zellen_status = koor_feld_alt[n];
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
+        else // Alle Felder außer Randfelder
+        {
+            zellen_status = koor_feld_alt[n];
+
+            if (counter == 3)
+            {
+                koor_feld_alt[n]=1;
+            }
+            else if (counter == 2)
+            {
+                koor_feld_neu[n] = koor_feld_alt[n];
+            }
+            else
+            {
+                koor_feld_neu[n] = 0;
+            }
+            counter = 0;
+        }
     }
 }
 
